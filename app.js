d0c5bb88edf9ad47f540391d475a082d3b31500c
mohammed199221 <>
2021-06-16 12:48:04 +0000

    add java Secript fill

---
+++ app.js
@@ -0,0 +1,27 @@
+alert ("Welecom to Ramadan-TV-shows")
+var name =prompt("Whats Your Name");
+alert ("Welecom"+name)
+var bestTV= prompt("Whats the best tv shows in ramadan 1- Nasel alagrab 2-alnemer 3-musa"); 
+alert ("Thanks for your choise" +bestTV ); 
+console.log("the user choice the"+bestTV );
+
+if (bestTV=="Nasel alagrab")
+{
+document.write("Nasel alagrab ++1")
+}
+else if(bestTV=="alnemer")
+{
+document.write("alnemer ++1")
+}
+else if (bestTV=="musa")
+{
+document.write("musa ++1")
+}
+else 
+{
+confirm ("Please select Just one of three choices 1- Nasel alagrab 2-alnemer 3-musa")
+}
+
+
+
+
